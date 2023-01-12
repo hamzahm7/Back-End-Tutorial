344af94e0351e11aac924f5f7c67c62df53c9dcf
hamzahm7 <99897730+hamzahm7@users.noreply.github.com>
2023-01-12 13:53:07 -0500

    Add files via upload

---
+++ node_https.js
@@ -0,0 +1,18 @@
+const { readdirSync } = require('fs');
+const http = require('http');
+const server = http.createServer((req,res)=> {
+    if (req.url === '/'){
+        res.write('Hello world');
+        res.write("This is our first server");
+        res.end();
+    }
+    if(req.url === '/api/courses')
+    {
+        res.write(JSON.stringify([1,2,3]));
+        res.write('This is a list of offerings');
+        res.write(' at BTHS');
+        res.end();
+    }
+});
+server.listen(3000);
+console.log("Listening on port 3000 ...");
